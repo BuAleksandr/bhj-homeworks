c4b83fcd2286c473a856aefbabbeeb9818e96087
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2022-04-27 20:35:20 +1100

    Update task.js

---
+++ js-features/countdown/task.js
@@ -0,0 +1,11 @@
+const timer = function () {
+  const p = document.getElementById('main');
+  
+  p.textContent = Number(p.textContent) - 1;
+    if ((p.textContent) <= 0) {
+      alert('Вы победили!');
+      clearInterval(inervalID);
+    } 
+  }
+
+const inervalID = setInterval(timer, 1000);
