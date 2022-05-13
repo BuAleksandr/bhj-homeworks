00e810b1c90d5f3fbf34bdadfc8f10b494cde1c1
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2022-05-13 23:03:41 +1100

    Update task.js
    add registerEvents()

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,15 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener("keydown", e => {
+      const currentSymbol = this.currentSymbol.textContent;
+        if (currentSymbol === e.key) {
+          this.success();
+        } else {
+          this.fail();
+        }
+      });
   }
-
+    
   success() {
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
